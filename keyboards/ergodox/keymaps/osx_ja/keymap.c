2d36ea50d8e6950bced82395114570a968267c8e
hilotter <hilotter3@gmail.com>
2016-08-16 23:11:16 +0900

    change lcmd to lctrl

--- keyboards/ergodox/keymaps/osx_ja/keymap.c
+++ keyboards/ergodox/keymaps/osx_ja/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L2  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCmd   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
+ * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
  * |--------+------+------+------+------+------| LCmd |           | RCmd |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_EQL,         KC_1,            KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
         KC_TAB,         KC_Q,            KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
-        KC_LGUI,        KC_A,            KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LCTL,        KC_A,            KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),     KC_X,   KC_C,   KC_V,   KC_B,   KC_LGUI,
         LT(SYMB,KC_GRV),LT(MDIA,KC_QUOT),KC_LGUI,  KC_LEFT,KC_RGHT,
                                               ALT_T(KC_APP),  KC_ESC,
